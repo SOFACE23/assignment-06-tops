1282f0b140bcf43d3a79b2c7df46183279c1865c
AlanHaman <114674762+AlanHaman@users.noreply.github.com>
2023-03-07 15:24:13 +0100

    Update daytime_server.cpp

--- src/daytime_server.cpp
+++ src/daytime_server.cpp
@@ -11,11 +11,11 @@
 #include <ctime>
 #include <iostream>
 #include <string>
-#include <boost/asio.hpp>
+#include <boost/asio.hpp> # library for I/O operations
 
-using boost::asio::ip::tcp;
+using boost::asio::ip::tcp; # create an alias for the tcp protocol 
 
-std::string make_daytime_string()
+std::string make_daytime_string() # This function creates a string that represent the live data and time.
 {
   using namespace std; // For time_t, time and ctime;
   time_t now = time(0);
@@ -26,21 +26,22 @@ int main()
 {
   try
   {
-    boost::asio::io_context io_context;
+    boost::asio::io_context io_context; # creating an io_context (I/O functionality)
 
     tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 13));
 
     while (true)
     {
       tcp::socket socket(io_context);
-      acceptor.accept(socket);
+      acceptor.accept(socket); # accepting connections from clients and creating new tcp,socet objects, each time there is a connection
 
-      std::string message = make_daytime_string();
+      std::string message = make_daytime_string(); # create message (containing data and time) and send it back to the client
 
       boost::system::error_code ignored_error;
       boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
     }
   }
+  # Here the program look for the problems that may occur during the exeution and prints out the message.
   catch (std::exception &e)
   {
     std::cerr << e.what() << std::endl;
